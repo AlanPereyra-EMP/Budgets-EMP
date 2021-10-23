3ac4546e34c2b53b0fd890e223c0b242d2cb7853
Alan Pereyra <alanpereyra33@gmail.com>
2021-10-23 12:43:30 -0300

    1.2.2

--- Budgets-EMP.php
+++ Budgets-EMP.php
@@ -7,7 +7,7 @@ Author: Empralidad
 Author URI: https://empralidad.com.ar/
 Text Domain: bemp
 License: GNU GENERAL PUBLIC LICENSE Version 3
-Version: 1.2.1
+Version: 1.2.2
 */
 if ( ! defined( 'ABSPATH' ) ){
 	exit;
